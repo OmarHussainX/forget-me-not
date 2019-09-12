f126cea509a34dd570b287c47901b542413a82e7
Omar Hussain <omar.hussain@gmail.com>
2019-09-12 13:08:53 -0600

    added basic views (with bootstrap styling) for two routes

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 import express from 'express'
 import exphbs from 'express-handlebars'
- 
 
 const app = express()
 const port = 5000
+
+// express-handlebars middleware
+// using the package's engine factory function, using the default
+// of 'main' for the name of the default template
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
 
-// 'root' route
+// 'index' route
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  // res.send('Hello World..')
+  const title = 'Welcome'
+  res.render('index', {
+    title: title
+  })
 })
 
 // 'about' route
 app.get('/about', (req, res) => {
-  res.send('About...')
+  res.render('about')
 })
 
 app.listen(port, () => {
