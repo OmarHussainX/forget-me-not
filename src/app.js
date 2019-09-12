4fbfb41d6927d8b0361e0c2ec806a28ce91e1fc1
Omar Hussain <omar.hussain@gmail.com>
2019-09-12 16:15:50 -0600

    using mongoose to connect to database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 import express from 'express'
 import exphbs from 'express-handlebars'
+import mongoose from 'mongoose'
 
 const app = express()
 const port = 5000
+
+
+// Use mongoose to establish DB connection,
+// logging any connection errors
+mongoose.connect('mongodb://localhost/forgetmenot-dev', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('DB connection established'))
+  .catch(error => console.log(`** DB connection error **: ${error}\n`))
+
+
+// To handle errors after initial connection was established,
+// listen for 'error' events on the connection
+const db = mongoose.connection
+db.on('error', error => console.log(`DB error: ${error}\n`))
+
+db.once('open', () => {
+  // we're connected!
+  console.log('DB open')
+})
 
 // express-handlebars middleware
 // using the package's engine factory function, using the default
